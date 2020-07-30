3336e50e73e4c58569c971754cb1aa895b631d83
Angga <angga@linuxmail.org>
2020-07-30 23:23:06 +0700

    stickers: kang: fix image size
    Change-Id: Ia640c231d2f68795636dfaf80c24f3a09da6027c
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -6,6 +6,7 @@
 """ Userbot module for kanging stickers or making new ones. Thanks @rupansh"""
 import io
 import math
+import os
 import random
 import urllib.request
 from os import remove
@@ -19,7 +20,7 @@ from telethon.tl.types import (
     MessageMediaPhoto,
 )
 
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
 
 KANGING_STR = [
@@ -256,26 +257,19 @@ async def kang(args):
 async def resize_photo(photo):
     """ Resize the given photo to 512x512 """
     image = Image.open(photo)
-    if (image.width and image.height) < 512:
-        size1 = image.width
-        size2 = image.height
-        if size1 > size2:
-            scale = 512 / size1
-            size1new = 512
-            size2new = size2 * scale
-        else:
-            scale = 512 / size2
-            size1new = size1 * scale
-            size2new = 512
-        size1new = math.floor(size1new)
-        size2new = math.floor(size2new)
-        sizenew = (size1new, size2new)
-        image = image.resize(sizenew)
+    maxsize = (512, 512)
+    if max(image.width, image.height) < 512:
+        scale = 512 / max(image.width, image.height)
+        new_size = (int(image.width * scale), int(image.height * scale))
+        image = image.resize(new_size, Image.ANTIALIAS)
     else:
-        maxsize = (512, 512)
         image.thumbnail(maxsize)
-
-    return image
+    os.remove(photo)
+    photo = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "sticker.png")
+    if os.path.exists(photo):
+        os.remove(photo)
+    image.save(photo, "PNG")
+    return photo
 
 
 @register(outgoing=True, pattern=r"^\.stkrinfo$")
